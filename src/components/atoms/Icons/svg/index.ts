07809cb5ebf69414c99b7eb4d05c83b483624249
Tanaca <jtanacar@gmail.com>
2022-07-09 04:35:43 -0300

    feat: add arrow right icon

--- src/components/atoms/Icons/svg/index.ts
+++ src/components/atoms/Icons/svg/index.ts
@@ -2,5 +2,12 @@ import Done from "./done";
 import Loading from "./loading";
 import Process from "./process";
 import Url from "./url";
+import ArrowRight from "./arrowRight";
 
-export default { loading: Loading, process: Process, url: Url, done: Done };
+export default {
+    loading: Loading,
+    process: Process,
+    url: Url,
+    done: Done,
+    arrowRight: ArrowRight,
+};
