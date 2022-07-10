2a1665ed7cb207e7e7524a949c6314a2362c4f53
Tanaca <jtanacar@gmail.com>
2022-07-10 07:22:50 -0300

    test: add test to removeHostname

--- src/utils/helpers/index.test.tsx
+++ src/utils/helpers/index.test.tsx
@@ -1,4 +1,8 @@
+import { removeHostname } from "./removeHostname";
 import { sleep } from "./sleep";
+
+const MOCK_RESULT = "MockResult";
+const MOCK_URL = `http://localhost:8080/api/${MOCK_RESULT}`;
 
 it("must validate if the sleep function is paralyzing for the requested time", async () => {
     const start = performance.now();
@@ -6,4 +10,9 @@ it("must validate if the sleep function is paralyzing for the requested time", a
     const end = performance.now() - start;
 
     expect(end).toBeGreaterThanOrEqual(100);
+});
+
+it("must validate if the removeHostname function is correctly removing all the hostname and leaving only the information in the last slash", async () => {
+    const result = removeHostname(MOCK_URL);
+    expect(result).toEqual(MOCK_RESULT);
 });
