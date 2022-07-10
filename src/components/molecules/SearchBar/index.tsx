3da5236d9d9ccdc7f062566942efdea355c6760c
Tanaca <jtanacar@gmail.com>
2022-07-10 06:34:05 -0300

    chore: add title in SearchBar component

--- src/components/molecules/SearchBar/index.tsx
+++ src/components/molecules/SearchBar/index.tsx
@@ -1,38 +1,39 @@
 import Button from "@atoms/Button";
+import Head from "@atoms/Head";
 import Input from "@atoms/Input";
 import React, { forwardRef, useRef, useImperativeHandle } from "react";
 import { Container } from "./styles";
 
-const SearchBar = forwardRef<
-    HTMLInputElement | null,
-    {
-        onSubmit?: (param?: string) => void;
-        collapse?: boolean;
+type Props = {
+    onSubmit?: (param?: string) => void;
+    collapse?: boolean;
+    title?: string;
+};
+
+const SearchBar = forwardRef<HTMLInputElement | null, Props>(
+    ({ onSubmit, collapse, title }, ref) => {
+        const inputRef = useRef<HTMLInputElement>(null);
+
+        const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            event.stopPropagation();
+            const value = inputRef?.current?.value;
+            onSubmit?.(value);
+            event.currentTarget.reset();
+        };
+
+        useImperativeHandle(ref, () => inputRef?.current as HTMLInputElement);
+
+        return (
+            <Container className={collapse ? "collapse" : ""}>
+                {title && <Head level="1">{title}</Head>}
+                <form onSubmit={handleSubmit}>
+                    <Input placeholder="Digite um termo..." ref={inputRef} />
+                    <Button type="submit">Procurar</Button>
+                </form>
+            </Container>
+        );
     }
->(({ onSubmit, collapse }, ref) => {
-    const inputRef = useRef<HTMLInputElement>(null);
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        event.stopPropagation();
-        const value = inputRef?.current?.value;
-        onSubmit?.(value);
-        event.currentTarget.reset();
-    };
-
-    useImperativeHandle(ref, () => inputRef?.current as HTMLInputElement);
-
-    return (
-        <Container>
-            <form
-                className={collapse ? "collapse" : ""}
-                onSubmit={handleSubmit}
-            >
-                <Input placeholder="Digite um termo..." ref={inputRef} />
-                <Button type="submit">Procurar</Button>
-            </form>
-        </Container>
-    );
-});
+);
 
 export default SearchBar;
--- src/components/molecules/SearchBar/styles.ts
+++ src/components/molecules/SearchBar/styles.ts
@@ -3,19 +3,26 @@ import styled from "styled-components";
 export const Container = styled.div`
     padding: 0 1em;
     box-sizing: border-box;
+    width: 100%;
+    height: 100vh;
+    display: inline-flex;
+    gap: 4em;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    &.collapse {
+        gap: 2em;
+        height: 30vh;
+    }
     form {
         width: 100%;
-        height: 100vh;
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
         transition: all 0.5s ease-in-out;
+        display: flex;
+        justify-content: center;
         box-sizing: border-box;
-        &.collapse {
-            height: 30vh;
-        }
+
         .input-container {
-            width: 60%;
+            width: 100%;
             input {
                 border-top-right-radius: 0;
                 border-bottom-right-radius: 0;
